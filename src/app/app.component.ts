4d99dceafc340454624852060fe9d274ea88ba34
Josiassantos1577 <josiassantos1577@gmail.com>
2025-01-05 01:47:28 -0300

    feat: Componentizar app da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes-angular';
+  navItems = [
+    { label: 'Início', link: '/feed', title: 'Ir para Início' },
+    { label: 'Minha rede', link: '/mynetwork', title: 'Minha rede' },
+    { label: 'Vagas', link: '/jobs', title: 'Vagas' },
+    { label: 'Mensagens', link: '/messaging', title: 'Mensagens' },
+    { label: 'Notificações', link: '/notifications', title: 'Notificações' },
+  ];
 }
